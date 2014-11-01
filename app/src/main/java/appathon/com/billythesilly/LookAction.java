075b6474a3a6d79253b0bd433e5b7d0559a6ea00
Blake Hawkins <b.hawkins@sms.ed.ac.uk>
2014-11-01 21:56:24 +0000

    add action implementations

---
+++ app/src/main/java/appathon/com/billythesilly/LookAction.java
@@ -0,0 +1,19 @@
+package appathon.com.billythesilly;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Blake on 11/1/2014.
+ */
+public class LookAction extends Action {
+    public LookAction(String description, Drawable icon){
+        this.description = description;
+        this.icon = icon;
+    }
+
+}
---
+++ app/src/main/java/appathon/com/billythesilly/WalkAction.java
@@ -0,0 +1,18 @@
+package appathon.com.billythesilly;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Blake on 11/1/2014.
+ */
+public class WalkAction extends Action {
+    public WalkAction(String description, Drawable icon){
+        this.description = description;
+        this.icon = icon;
+    }
+}
