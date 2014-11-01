fb5146d9a804e9bd8a0b62ec2546c48eb3e67403
LisaXie <lxie.ger@gmail.com>
2014-11-01 20:51:11 +0000

    Autogenerate times helped etc.

--- app/src/main/java/appathon/com/billythesilly/Stats.java
+++ app/src/main/java/appathon/com/billythesilly/Stats.java
@@ -1,19 +1,38 @@
 package appathon.com.billythesilly;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class Stats extends Activity {
+    private int rewardsTotal,timesTotal,starsTotal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        rewardsTotal = timesTotal = starsTotal = 0;
         setContentView(R.layout.activity_stats);
+        ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar);
+        progress.setProgress(0);
+        generateText();
+
     }
 
+    public void generateText() {
+        TextView stars = (TextView) this.findViewById(R.id.StarsEarned);
+        TextView times = (TextView) this.findViewById(R.id.TimesHelped);
+        TextView rewards = (TextView) this.findViewById(R.id.RewardsEarned);
+        stars.setText("x " + String.valueOf(starsTotal));
+        times.setText("x " + String.valueOf(timesTotal));
+        rewards.setText("x " + String.valueOf(rewardsTotal));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
