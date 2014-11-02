c628d22b1df3025ed83618a13bfc7d30ad609cff
LisaXie <lxie.ger@gmail.com>
2014-11-02 05:15:04 +0000

    Added Activity in Manifesto

--- app/src/main/java/appathon/com/billythesilly/scenario/ReactionScenarioCookieActivity.java
+++ app/src/main/java/appathon/com/billythesilly/scenario/ReactionScenarioCookieActivity.java
@@ -3,6 +3,7 @@ package appathon.com.billythesilly.scenario;
 import android.view.View;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import appathon.com.billythesilly.R;
 
@@ -15,6 +16,19 @@ abstract public class ReactionScenarioCookieActivity extends ReactionScenarioAct
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reaction_scenario_cookie);
+
+    }
+
+    public void goDown(View v) {
+        // Change Billy's stance
+    }
+
+    public void take(View v) {
+        // give Billy cookies
+        ImageView billy = (ImageView) findViewById(R.id.BillyHangingCookie);
+        ImageView cookies = (ImageView) findViewById(R.id.MovableCookies);
+        cookies.setImageResource(android.R.color.transparent);
+        billy.setImageResource(R.drawable.billycookie);
 
     }
 }
