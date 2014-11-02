68715fc5e9a5469e571958a0d8ad283134ccfb4a
FilipeWang <filipe.wang@gmail.com>
2014-11-02 04:09:46 +0000

    added color change to scenario number

--- app/src/main/java/appathon/com/billythesilly/ScenariosScreen.java
+++ app/src/main/java/appathon/com/billythesilly/ScenariosScreen.java
@@ -1,11 +1,13 @@
 package appathon.com.billythesilly;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 
 import appathon.com.billythesilly.scenario.ScenarioCrossStreetActivity;
 //Line that changes color of text:     current1.setTextColor(Color.BLUE);
@@ -15,8 +17,14 @@ public class ScenariosScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        boolean [] check = checkComplete();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenarios_screen);
+        for(int i=0; i<18; i++){
+            if(check[i]){
+                changeColor(i);
+            }
+        }
     }
 
 
@@ -132,5 +140,91 @@ public class ScenariosScreen extends Activity {
         startActivity(intent);
     }
 
+    public boolean [] checkComplete(){
+        boolean [] check = new boolean[18];
+        for(int i=0; i<18; i++){
+            check[i] = true;
+        }
+        return check;
+    }
 
+       public void changeColor(int n){
+        switch(n){
+            case 1:
+                Button hold1 = (Button) findViewById(R.id.button1);
+                hold1.setTextColor(Color.GREEN);
+                break;
+            case 2:
+                Button hold2 = (Button) findViewById(R.id.button2);
+                hold2.setTextColor(Color.GREEN);
+                break;
+            case 3:
+                Button hold3 = (Button) findViewById(R.id.button3);
+                hold3.setTextColor(Color.GREEN);
+                break;
+            case 4:
+                Button hold4 = (Button) findViewById(R.id.button4);
+                hold4.setTextColor(Color.GREEN);
+                break;
+            case 5:
+                Button hold5 = (Button) findViewById(R.id.button5);
+                hold5.setTextColor(Color.GREEN);
+                break;
+            case 6:
+                Button hold6 = (Button) findViewById(R.id.button6);
+                hold6.setTextColor(Color.GREEN);
+                break;
+            case 7:
+                Button hold7 = (Button) findViewById(R.id.button7);
+                hold7.setTextColor(Color.GREEN);
+                break;
+            case 8:
+                Button hold8 = (Button) findViewById(R.id.button8);
+                hold8.setTextColor(Color.GREEN);
+                break;
+            case 9:
+                Button hold9 = (Button) findViewById(R.id.button9);
+                hold9.setTextColor(Color.GREEN);
+                break;
+            case 10:
+                Button hold10 = (Button) findViewById(R.id.button10);
+                hold10.setTextColor(Color.GREEN);
+                break;
+            case 11:
+                Button hold11 = (Button) findViewById(R.id.button11);
+                hold11.setTextColor(Color.GREEN);
+                break;
+            case 12:
+                Button hold12 = (Button) findViewById(R.id.button12);
+                hold12.setTextColor(Color.GREEN);
+                break;
+            case 13:
+                Button hold13 = (Button) findViewById(R.id.button13);
+                hold13.setTextColor(Color.GREEN);
+                break;
+            case 14:
+                Button hold14 = (Button) findViewById(R.id.button14);
+                hold14.setTextColor(Color.GREEN);
+                break;
+            case 15:
+                Button hold15 = (Button) findViewById(R.id.button15);
+                hold15.setTextColor(Color.GREEN);
+                break;
+            case 16:
+                Button hold16 = (Button) findViewById(R.id.button16);
+                hold16.setTextColor(Color.GREEN);
+                break;
+            case 17:
+                Button hold17 = (Button) findViewById(R.id.button17);
+                hold17.setTextColor(Color.GREEN);
+                break;
+            case 18:
+                Button hold18 = (Button) findViewById(R.id.button18);
+                hold18.setTextColor(Color.GREEN);
+                break;
+            default:
+                break;
+        }
+
+    }
 }
