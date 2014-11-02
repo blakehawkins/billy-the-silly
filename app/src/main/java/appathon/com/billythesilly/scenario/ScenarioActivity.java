2d04cf73fc51ad40e56f501023288d17f2a5c13b
Blake Hawkins <b.hawkins@sms.ed.ac.uk>
2014-11-02 02:46:51 +0000

    show street :3

--- app/src/main/java/appathon/com/billythesilly/scenario/ScenarioActivity.java
+++ app/src/main/java/appathon/com/billythesilly/scenario/ScenarioActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
@@ -20,6 +22,13 @@ abstract public class ScenarioActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenario);
+
+        LinearLayout slotLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams
+                .MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        slotLayout.setLayoutParams(lp);
+        slotLayout.setWeightSum(0.0f);
+        slotLayout.setOrientation(LinearLayout.HORIZONTAL);
     }
 
     @Override
@@ -32,6 +41,10 @@ abstract public class ScenarioActivity extends Activity {
     }
 
     abstract protected void initializeTopBarMembers(Context cxt);
+
+    abstract protected void initializeTargets(Context cxt);
+
+    abstract protected void drawSprites(Context cxt);
 
     abstract public void grade(View view);
 }
