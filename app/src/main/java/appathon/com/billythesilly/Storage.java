792b08f06710ad12160c3c83aacabe7e046c35f2
scriptinglife1 <oscarpost@hotmail.fr>
2014-11-02 06:08:32 +0000

    convention

--- app/src/main/java/appathon/com/billythesilly/Storage.java
+++ app/src/main/java/appathon/com/billythesilly/Storage.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
  * Created by Oscar on 02/11/2014.
  */
 public class Storage {
-    public static ArrayList<Integer> rewards = new ArrayList<Integer>();
-    public static int stars = 0;
-    public static boolean[] levels = { false, false, false,false, false, false,
+    public static ArrayList<Integer> REWARDS = new ArrayList<Integer>();
+    public static int STARS = 0;
+    public static boolean[] LEVELS = { false, false, false,false, false, false,
             false, false, false, false, false, false, false, false, false, false, false};
 
 }
