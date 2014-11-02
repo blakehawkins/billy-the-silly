1fcf33dd503706d9be5941a6a646e5913f5c5e19
Blake Hawkins <b.hawkins@sms.ed.ac.uk>
2014-11-02 09:27:35 +0000

    scenario activity add padding to top bar

--- app/src/main/java/appathon/com/billythesilly/scenario/TopBarMember.java
+++ app/src/main/java/appathon/com/billythesilly/scenario/TopBarMember.java
@@ -4,9 +4,14 @@ package appathon.com.billythesilly.scenario;
 
 import android.content.Context;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 abstract public class TopBarMember extends Button {
-    TopBarMember(Context cxt){
+    TopBarMember(Context cxt) {
         super(cxt);
+        LinearLayout.LayoutParams lm = new LinearLayout.LayoutParams(LinearLayout.LayoutParams
+                .WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lm.setMargins(0, 0, 10, 0);
+        setLayoutParams(lm);
     }
 }
